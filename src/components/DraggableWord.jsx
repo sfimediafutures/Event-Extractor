da8de29ba2e0f8b274be98d62194c08ee3f17509
Svenja Forstner <cuf014@uib.no>
2024-10-09 17:01:24 +0200

    change pointers for inactive and active state

--- src/components/DraggableWord.jsx
+++ src/components/DraggableWord.jsx
@@ -15,8 +15,8 @@ const DraggableWord = ({ word, disabled }) => {
       onDragStart={handleDragStart}
       className={`inline-block px-2 py-1 rounded m-1 ${
         disabled
-          ? 'bg-gray-200 text-gray-500 cursor-not-allowed'
-          : 'bg-blue-100 text-blue-800 cursor-move'
+          ? 'bg-gray-200 text-gray-500 cursor-auto'
+          : 'bg-blue-100 text-blue-800 cursor-grab'
       }`}
     >
       {word}
