2b9fd97d9bfa79bf9e44ac97f0fb180262a46aed
Svenja Forstner <cuf014@uib.no>
2024-10-24 11:46:16 +0200

    added fontawesome home icon

--- src/components/StartOverlay.jsx
+++ src/components/StartOverlay.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const handleBackToStart = () => {
     window.location.href = '/';
@@ -11,16 +13,16 @@ const Overlay = ({ instruction, onStart, onClose }) => {
         <h2 className="text-xl font-bold mb-4">Din oppgave</h2>
         <p className="mb-4">{instruction}</p>
         <button
+          className="mr-4 bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
+          onClick={handleBackToStart}
+        >
+          <FontAwesomeIcon icon={faHouse} />
+        </button>
+        <button
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
           onClick={onStart}
         >
           Start
-        </button>
-        <button
-          className="ml-4 bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
-          onClick={handleBackToStart}
-        >
-          Back to Selection
         </button>
       </div>
     </div>
