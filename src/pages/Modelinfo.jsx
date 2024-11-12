23a6d90db585d0e9aa634dee7b1b4db25db221ff
Svenja Forstner <cuf014@uib.no>
2024-11-12 16:47:58 +0100

    more translation

--- src/pages/Modelinfo.jsx
+++ src/pages/Modelinfo.jsx
@@ -6,17 +6,16 @@ const ModelInfo = () => {
     <div className="max-w-2xl w-full">
       <h1 className="text-3xl font-bold mb-4">Event Extractor Model</h1>
       <p className="mb-4">
-        The Event Extractor model, developed by <a href="https://mediafutures.no/2021/10/18/huiling-you/" target="_blank" style={{ color: 'blue' }}>Huiling You</a>, is a natural language processing model designed to quickly and accurately identify key events and important information
-        from text. It can fastly process articles and extract relevant information,
-        significantly reducing the time needed for manual analysis.
+        Event Extractor, utviklet av <a href="https://mediafutures.no/2021/10/18/huiling-you/" target="_blank" style={{ color: 'blue' }}>Huiling You</a>, er en natural language processing model som er utviklet for å nøyaktig identifisere events og viktig informasjon
+        fra tekst. Den kan raskt behandle artikler og trekke ut relevant informasjon, og dermed redusere tiden som trengs for manuell analyse.
       </p>
       <p className="mb-4">
         Key features:
       </p>
       <ul className="list-disc pl-5 mb-4">
-        <li>Rapid processing (4 seconds per article)</li>
-        <li>High accuracy in identifying important information</li>
-        <li>Ability to handle various text formats, lengths and languages</li>
+        <li>Rask behandling (4 sekunder per artikkel)</li>
+        <li>Høy nøyaktighet i identifisering av viktig informasjon</li>
+        <li>Evne til å håndtere ulike tekstformater, lengder og språk</li>
       </ul>
     </div>
   );
